dc01d4d29121161892edc4615e24556a16c56944
glentechkenya <glentech287@gmail.com>
2026-02-03 17:41:53 +0300

    Create sample data for movies and trailers
    Added sample data for movie downloads, searches, trending items, and trailers.

---
+++ sample-data.js
@@ -0,0 +1,66 @@
+export const movieDownloadSample = {
+  "status": true,
+  "creator": "Keithkeizzah",
+  "result": {
+    "book_id": "41000105764",
+    "episode": "1",
+    "video_url": "https://hwztvideo.dramaboxdb.com/02/4x6/46x7/467x5/46750100014/578668220_3/578668220.720p.narrowv3.mp4?Expires=1770192000&Signature=U2fGJLcerkSf7c2KQey0Mp~6fsgQ5PfjdQNMepmzQelm5ram3q2fMSpJookHriZ-wcqzZ53enc0N0tZMi2QkmqrulH-yUIg5aG~s4N6pP06DZfoXC4h36lNWh7XTHUE6hYltNzvu4V3NtYoSEC5G9pzI2iNqUSlyrF1zZ9uz5KePgZmC6cD9QGBHyCNlkfBpknuL4vjiEiIYaDpBL8MABMzarFMIEgDv3pdmfMllaLxNOBvB-ApnkEIWP8esfXSK40s-wRgHLTZ5QIuDGSrEPPfcwDQPW3AQcB08gc7-rZ1zHsM7MY-fMBoaxbxjk9nCrfzfzS4rnnx6R~4q3evSyA__&Key-Pair-Id=K3HA2TLE2QH99V"
+  }
+};
+
+export const movieSearchSample = {
+  "status": true,
+  "creator": "Keithkeizzah",
+  "result": {
+    "keyword": "as good as dead",
+    "count": 18,
+    "results": [
+      {"title":"As Good as Dead","url":"https://moviebox.ph/detail/as-good-as-dead-oM2Qo1hIWD2","rating":"5.55.5","type":"movie"},
+      {"title":"Dead Good","url":"https://moviebox.ph/detail/dead-good-AC2GnnkTkL7","rating":"8.08.0","type":"movie"},
+      {"title":"4Got10","url":"https://moviebox.ph/detail/4got10-izTAjKRQfb7","rating":"4.14.1","type":"movie"},
+      {"title":"The Dead South Songs Playlist [FULL ALBUM] In Hell I'll Be In Good Company, Spaghetti, Broken Cowboy","url":"https://moviebox.ph/detail/the-dead-south-songs-playlist-full-album-in-hell-ill-be-in-good-company-spaghetti-broken-cowboy-QgJ822mSQ03","rating":"7.37.3","type":"music"},
+      {"title":"The Dead South - In Hell I'll Be In Good Company [with lyrics]","url":"https://moviebox.ph/detail/the-dead-south-in-hell-ill-be-in-good-company-with-lyrics-KRlPZ54yDda","rating":"7.57.5","type":"movie"},
+      {"title":"The Dead South - The Good Lord (Live)","url":"https://moviebox.ph/detail/the-dead-south-the-good-lord-live-Cv0D74wOXga","rating":"7.17.1","type":"movie"},
+      {"title":"The Dead South perform \"In Hell I'll Be in Good Company\" on DittyTV","url":"https://moviebox.ph/detail/the-dead-south-perform-in-hell-ill-be-in-good-company-on-dittytv-aHnoFk9csP4","rating":"7.07.0","type":"movie"},
+      {"title":"The Dead South - In Hell I'll Be In Good Company  - Lyrics","url":"https://moviebox.ph/detail/the-dead-south-in-hell-ill-be-in-good-company-lyrics-2FKSERxhyJ","rating":"7.57.5","type":"movie"},
+      {"title":"The Dead South - In Hell I'll Be In Good Company @ The Bluebird Cafe","url":"https://moviebox.ph/detail/the-dead-south-in-hell-ill-be-in-good-company-at-the-bluebird-cafe-0uDe00mefk9","rating":"7.57.5","type":"movie"},
+      {"title":"The Dead South - In Hell I'll Be In Good Company [Official Music Video]","url":"https://moviebox.ph/detail/the-dead-south-in-hell-ill-be-in-good-company-official-music-video-6VFBPJeAzo5","rating":"7.67.6","type":"music"},
+      {"title":"As Beautiful as You","url":"https://moviebox.ph/detail/as-beautiful-as-you-OHdIesIQzr5","rating":"7.77.7","type":"movie"},
+      {"title":"Barbie as Rapunzel","url":"https://moviebox.ph/detail/barbie-as-rapunzel-WNspjyy2Am9","rating":"6.66.6","type":"movie"},
+      {"title":"Jab Tak Hai Jaan","url":"https://moviebox.ph/detail/jab-tak-hai-jaan-2H9VZeriYMa","rating":"6.76.7","type":"movie"},
+      {"title":"Savvy Sheldon Feels Good As Hell","url":"https://moviebox.ph/detail/savvy-sheldon-feels-good-as-hell-yB85RMbRl1a","rating":"7.37.3","type":"movie"},
+      {"title":"AS IT SEEMS","url":"https://moviebox.ph/detail/as-it-seems-MaTziRsEFP2","rating":"00","type":"movie"},
+      {"title":"As Verdades","url":"https://moviebox.ph/detail/as-verdades-yZGi3NvzC61","rating":"5.85.8","type":"movie"},
+      {"title":"White as Snow","url":"https://moviebox.ph/detail/white-as-snow-AI9wUnLycG1","rating":"5.45.4","type":"movie"},
+      {"title":"WILD AS FIRE","url":"https://moviebox.ph/detail/wild-as-fire-sQvAgiceyp6","rating":"00","type":"movie"}
+    ]
+  }
+};
+
+export const latestTrendingSample = {
+  "status": true,
+  "creator": "Keithkeizzah",
+  "result": {
+    "latest": [
+      {"title":"Istriku, Aku Memang Dewa (Sulih Suara)","book_id":null,"image":"https://hwztchapter.dramaboxdb.com/data/cppartner/4x2/42x0/420x0/42000005442/42000005442.jpg?t=1770020068636","views":"ep","episodes":"ep"},
+      {"title":"Suami yang Diremehkan","book_id":null,"image":"https://hwztchapter.dramaboxdb.com/data/cppartner/4x2/42x0/420x0/42000004864/42000004864.jpg?t=1769414312425","views":"ep","episodes":"ep"},
+      {"title":"Di Cintamu Hanya Ada Dusta (Sulih Suara)","book_id":null,"image":"https://hwztchapter.dramaboxdb.com/data/cppartner/4x2/42x0/420x0/42000005043/42000005043.jpg?t=1769578790330","views":"ep","episodes":"ep"}
+    ],
+    "trending": [
+      {"rank":"1","title":"Ultimatum Sang Raja Mafia","book_id":null,"image":"https://hwztchapter.dramaboxdb.com/data/cppartner/4x2/42x0/420x0/42000003970/42000003970.jpg?t=1768293743402","views":"Ep"},
+      {"rank":"2","title":"Sang Tuan Muda Penyamar (Sulih Suara)","book_id":null,"image":"https://hwztchapter.dramaboxdb.com/data/cppartner/4x2/42x0/420x0/42000004216/42000004216.jpg?t=1768531629689","views":"Ep"}
+    ]
+  }
+};
+
+export const trailerSample = {
+  "status": true,
+  "creator": "Keithkeizzah",
+  "result": {
+    "url": "https://moviebox.ph/detail/as-beautiful-as-you-OHdIesIQzr5",
+    "title": "Watch As Beautiful as You Streaming Online on Moviebox",
+    "description": "Watch As Beautiful as You free streaming online on MoviesBox. Enjoy HD quality, no ads, and free downloads. Chronicles the ups and downs of Ji Xing as she navigates being an entrepreneur and falling in and out of love.",
+    "thumbnail": "https://pbcdnw.aoneroom.com/image/2026/01/26/3368d636ee17c54608768410e9769a4e.jpg",
+    "trailerUrl": "https://macdn.aoneroom.com/media/vone/2024/08/26/f6f0518548721f6c6b01733f21b4f7e2-sd.mp4"
+  }
+};
